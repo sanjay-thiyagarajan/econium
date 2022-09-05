45fb2585b9cdcdaaa24444091a42101214ff5b4a
Sanjay Thiyagarajan <sanjayipscoc@gmail.com>
2022-09-05 14:41:59 +0530

    Add basic panels

--- src/App.js
+++ src/App.js
@@ -3,13 +3,74 @@ import './App.css';
 import React from 'react';
 import mondaySdk from "monday-sdk-js";
 import "monday-ui-react-core/dist/main.css"
-import { Button } from "monday-ui-react-core";
+import { Button, Flex } from "monday-ui-react-core";
+import Modal from '@mui/material/Modal';
+import Box from '@mui/material/Box';
+
 
 function App() {
   const monday = mondaySdk();
+  const [openEmp, setOpenEmp] = React.useState(false);
+  const [openVeh, setOpenVeh] = React.useState(false);
+  const [openAllow, setOpenAllow] = React.useState(false);
+  const handleOpenEmp = () => setOpenEmp(true);
+  const handleOpenVeh = () => setOpenVeh(true);
+  const handleOpenAllow = () => setOpenAllow(true);
+  const handleCloseEmp = () => setOpenEmp(false);
+  const handleCloseVeh = () => setOpenVeh(false);
+  const handleCloseAllow = () => setOpenAllow(false);
+  
+  const style = {
+    position: 'absolute',
+    top: '50%',
+    left: '50%',
+    transform: 'translate(-50%, -50%)',
+    width: 400,
+    bgcolor: 'background.paper',
+    boxShadow: 24,
+    borderRadius: 2,
+    p: 4,
+  };
+  
   return (
     <div className="App">
-    <Button>Hello</Button>
+      <div className='panel-2'>
+        <div className='card'>
+          <header title='Manage' className="cardTitle">Leaderboard</header>
+        </div>
+        <div className='card'>
+          <header title='Manage' className="cardTitle">Manage</header>
+          <Button className='mgmtButtons' onClick = {handleOpenEmp} style={{backgroundColor: 'brown'}}>Add Employee</Button>
+          <Button className='mgmtButtons' onClick = {handleOpenVeh} style={{backgroundColor: 'green'}}>Add Vehicle</Button>
+          <Button className='mgmtButtons' onClick = {handleOpenAllow}>Add Allowance</Button>
+        </div>
+      </div>
+      <div className='card'>
+      </div>
+      <Modal
+        open={openEmp}
+        onClose={handleCloseEmp}
+      >
+        <Box sx={style}>
+          <header title='Manage' className="cardTitle">Add Employee</header>
+        </Box>
+      </Modal>
+      <Modal
+        open={openVeh}
+        onClose={handleCloseVeh}
+      >
+        <Box sx={style}>
+          <header title='Manage' className="cardTitle">Add Vehicle</header>
+        </Box>
+      </Modal>
+      <Modal
+        open={openAllow}
+        onClose={handleCloseAllow}
+      >
+        <Box sx={style}>
+          <header title='Manage' className="cardTitle">Add Allowance</header>
+        </Box>
+      </Modal>
     </div>
   );
 }
