36383034b015cbbf668aac032044df9f9076e485
Sanjay Thiyagarajan <sanjayipscoc@gmail.com>
2022-09-05 12:50:39 +0530

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import mondaySdk from "monday-sdk-js";
+import "monday-ui-react-core/dist/main.css"
+import { Button } from "monday-ui-react-core";
 
 function App() {
+  const monday = mondaySdk();
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <Button>Hello</Button>
     </div>
   );
 }
