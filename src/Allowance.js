5005d53c85aa8b192a66b36920169fffbd069a92
Sanjay Thiyagarajan <sanjayipscoc@gmail.com>
2022-09-07 16:48:22 +0530

    Fix data grid problem

--- src/Allowance.js
+++ src/Allowance.js
@@ -3,7 +3,7 @@ import './App.css';
 import { Button, TextField, Dropdown, Divider, Checkbox, Flex, SplitButton } from "monday-ui-react-core";
 import Modal from '@mui/material/Modal';
 import Box from '@mui/material/Box';
-// import { DataGrid, GridRowsProp, GridColDef  } from '@mui/x-data-grid';
+import { DataGrid } from '@mui/x-data-grid';
 export default function Allowance({setAllowance, allowance}) {
     const handleModal = () =>{
         setAllowance(false)
@@ -17,24 +17,28 @@ export default function Allowance({setAllowance, allowance}) {
         boxShadow: 24,
         borderRadius: 2,
         p: 4,
+        height: 'max-content',
+        width: 'max-content'
       };
-    //   const rows= [
-    //     { id: 1, col1: 'Hello', col2: 'World' },
-    //     { id: 2, col1: 'DataGridPro', col2: 'is Awesome' },
-    //     { id: 3, col1: 'MUI', col2: 'is Amazing' },
-    //   ];
+      const rows = [
+        { id: 1, start: 'Hello', end: 'World', bonus: 'a' },
+        { id: 2, start: 'DataGridPro', end: 'is Awesome', bonus: 'a' },
+        { id: 3, start: 'MUI', end: 'is Amazing', bonus: 'a' },
+      ];
       
-    //   const columns = [
-    //     { field: 'col1', headerName: 'Column 1', width: 150 },
-    //     { field: 'col2', headerName: 'Column 2', width: 150 },
-    //   ];
+      const columns = [
+        { field: 'start', headerName: 'Start', width: 150, editable: true },
+        { field: 'end', headerName: 'End', width: 150, editable: true },
+        { field: 'bonus', headerName: 'Bonus', width: 150, editable: true }
+      ];
   return (
     <Modal open={allowance}
     onClose={handleModal}>
         <Box sx={style}>
             <header className="cardTitle" title='Allowance Details'>Allowance Details</header>
+            <DataGrid columns={columns} rows={rows} autoHeight={true}/>
+            <Button style={{width: '100%', marginTop: 20}}>Save</Button>
         </Box>
-   
     </Modal>
   )
 }
